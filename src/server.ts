42302bb6892b23ce02d493712e73d1f94fbe3711
FelipePDS <felipepdasilva66@gmail.com>
2021-06-22 13:50:53 -0300

    adding json, router and database connection

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
-import express, { Request, Response } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+
+import { router } from './routes';
+import './database';
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => res.send('Olá!!!!!!'));
+app.use(express.json());
+
+app.use(router);
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
