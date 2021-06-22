c5edaf96006548e0343b321a7b94a57a41f9aa1f
FelipePDS <felipepdasilva66@gmail.com>
2021-06-22 13:49:48 -0300

    adding CreateUserController

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+import { CreateUserController } from './controllers/CreateUserController';
+
+const createUserController = new CreateUserController();
+
+router.post('/users', createUserController.handle);
+
+export { router };
