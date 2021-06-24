2eddb3ef47af6fdc4194012528fde0bd6d6f6340
FelipePDS <felipepdasilva66@gmail.com>
2021-06-24 18:08:12 -0300

    adding ComplimentRespository

---
+++ src/repositories/ComplimentRepository.ts
@@ -0,0 +1,7 @@
+import { Repository, EntityRepository } from 'typeorm';
+import { Compliment } from '../entities/Compliment';
+
+@EntityRepository(Compliment)
+class ComplimentRepository extends Repository<Compliment> {};
+
+export { ComplimentRepository };
