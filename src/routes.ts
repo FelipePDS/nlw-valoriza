21e3b5d9d8a7fe2b3c2eb3cb7be3f65ea1522f52
FelipePDS <felipepdasilva66@gmail.com>
2021-06-24 18:02:51 -0300

    adding AuthenticateUserController and CreateComplimentController

--- src/routes.ts
+++ src/routes.ts
@@ -4,14 +4,22 @@ const router = Router();
 
 import { CreateUserController } from './controllers/CreateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CreateComplimentController();
 
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
 router.post('/users', createUserController.handle);
 
+router.post('/login', authenticateUserController.handle);
+
 router.post('/tags', ensureAdmin, createTagController.handle);
+
+router.post('/compliments', createComplimentController.handle);
 
 export { router };
