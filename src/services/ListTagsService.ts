80400f22c1216b9550188e5fd6a96e84ab5de511
FelipePDS <felipepdasilva66@gmail.com>
2021-06-26 13:41:01 -0300

    add ListTags

---
+++ src/services/ListTagsService.ts
@@ -0,0 +1,18 @@
+import { getCustomRepository } from "typeorm";
+import { classToPlain } from 'class-transformer';
+
+import { TagRepository } from "../repositories/TagRepository";
+
+class ListTagsService {
+
+  async execute() {
+    const tagRepository = getCustomRepository(TagRepository);
+
+    const tags = await tagRepository.find();
+
+    return classToPlain(tags);
+  }
+
+}
+
+export { ListTagsService };
