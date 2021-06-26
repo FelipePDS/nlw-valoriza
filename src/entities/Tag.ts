1c6b0649d7f958051779a46c589d0a7f5443adb5
FelipePDS <felipepdasilva66@gmail.com>
2021-06-26 13:44:15 -0300

    add nameCustom attribute with class-transformer expose

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
+import { Expose } from 'class-transformer';
 
 @Entity('tags')
 class Tag {
@@ -14,6 +15,11 @@ class Tag {
 
   @CreateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'nameCustom' })
+  nameCustom(): string {
+    return `#${this.name}`;
+  }
 
   constructor() {
     if (!this.id) {
