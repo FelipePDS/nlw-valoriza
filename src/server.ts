68081ef7d662a622a59be5ecccd3a826a715c9bb
FelipePDS <felipepdasilva66@gmail.com>
2021-06-26 13:32:16 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import { router } from './routes';
 import './database';
@@ -8,6 +9,8 @@ import './database';
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
