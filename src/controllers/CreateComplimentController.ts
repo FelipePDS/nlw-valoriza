a158df8bc57774839de71c88fee94e5c645fbb0c
FelipePDS <felipepdasilva66@gmail.com>
2021-06-26 13:39:22 -0300

    change user_sender by logged in user

--- src/controllers/CreateComplimentController.ts
+++ src/controllers/CreateComplimentController.ts
@@ -6,16 +6,16 @@ class CreateComplimentController {
 
   async handle(request: Request, response: Response) {
     const {
-      user_sender, 
       user_receiver, 
       tag_id, 
       message 
     } = request.body;
+    const { user_id } = request;
 
     const createComplimentService = new CreateComplimentService();
 
     const compliment = await createComplimentService.execute({
-      user_sender, 
+      user_sender: user_id, 
       user_receiver,
       tag_id,
       message
